3907fd5215fd3531f201e0eb4d5669d81e80d668
cppbitman <daihongjun@kedacom.com>
2018-11-22 19:11:14 +0800

    refactor

--- conanfile.py
+++ conanfile.py
@@ -60,20 +60,19 @@ class FontconfigConan(ConanFile):
         git.clone(url_, branch=branch_)
 
     def build(self):
-        pkg_config_paths=[os.path.join(self.deps_cpp_info["freetype"].rootpath, "lib", "pkgconfig"),
-                          os.path.join(self.deps_cpp_info["expat"].rootpath, "lib", "pkgconfig")]
+        pkg_config_paths=[ os.path.join(self.deps_cpp_info[i].rootpath, "lib", "pkgconfig") for i in ["freetype","expat"] ]
         prefix = os.path.join(self.build_folder, self._build_subfolder, "install")
+        libpath = [ os.path.join(self.deps_cpp_info[i].rootpath, "lib") for i in ["bzip2","zlib","libpng"] ]
         meson = Meson(self)
 
         if self.settings.os == "Windows":
-            libpng = [ os.path.join(self.deps_cpp_info["libpng"].rootpath, i) for i in ["lib", "bin"] ]
-            bzip2 = [ os.path.join(self.deps_cpp_info["bzip2"].rootpath, i) for i in ["lib", "bin"] ]
-            zlib = [ os.path.join(self.deps_cpp_info["zlib"].rootpath, i) for i in ["lib", "bin"] ]
-            expat = [ os.path.join(self.deps_cpp_info["expat"].rootpath, i) for i in ["lib", "bin"] ]
-            freetype = os.path.join(self.deps_cpp_info["freetype"].rootpath, "bin")
+            libpath.extend([os.path.join(self.deps_cpp_info["expat"].rootpath, "lib")])
+            binpath = [ os.path.join(self.deps_cpp_info[i].rootpath, "bin") for i in ["bzip2","zlib","libpng","expat","freetype"] ]
 
-            with tools.environment_append({ 'LIB' : os.pathsep.join([libpng[0], bzip2[0], zlib[0], expat[0], os.getenv('LIB')]),
-                'PATH' : os.pathsep.join([os.path.join(prefix,"bin"),freetype,zlib[1],bzip2[1],libpng[1],expat[1],os.getenv('PATH')])}):
+            with tools.environment_append({
+                'LIB'  : os.pathsep.join(libpath + [os.getenv('LIB')]),
+                'PATH' : os.pathsep.join(binpath + [os.getenv('PATH'),os.path.join(prefix,"bin")])
+                }):
                 meson.configure(defs={'prefix' : prefix},
                                 source_dir=self._source_subfolder, build_dir=self._build_subfolder,
                                 pkg_config_paths=pkg_config_paths)
@@ -81,16 +80,10 @@ class FontconfigConan(ConanFile):
                 self.run('ninja -C {0} install'.format(meson.build_dir))
 
         if self.settings.os == "Linux":
-            pkg_config_paths.extend([os.path.join(self.deps_cpp_info["zlib"].rootpath, "lib", "pkgconfig"),
-                                     os.path.join(self.deps_cpp_info["bzip2"].rootpath, "lib", "pkgconfig"),
-                                     os.path.join(self.deps_cpp_info["libpng"].rootpath, "lib", "pkgconfig"),
-                                     os.path.join(self.deps_cpp_info["libuuid"].rootpath, "lib", "pkgconfig"),])
+            pkg_config_paths.extend([os.path.join(self.deps_cpp_info[i].rootpath, "lib", "pkgconfig") for i in ["zlib","bzip2","libpng","libuuid"] ] )
             path = [os.path.join(self.deps_cpp_info["gperf"].rootpath, "bin")]
             include = [os.path.join(self.deps_cpp_info["libuuid"].rootpath, "include")]
-            libpath = [os.path.join(self.deps_cpp_info["freetype"].rootpath, "lib"),
-                       os.path.join(self.deps_cpp_info["bzip2"].rootpath, "lib"),
-                       os.path.join(self.deps_cpp_info["zlib"].rootpath, "lib"),
-                       os.path.join(self.deps_cpp_info["libpng"].rootpath, "lib"),]
+            libpath.extend([os.path.join(self.deps_cpp_info["freetype"].rootpath, "lib")])
             with tools.environment_append({
                 'PATH' : os.pathsep.join(path+[os.getenv('PATH')]),
                 'C_INCLUDE_PATH' : os.pathsep.join(include),
